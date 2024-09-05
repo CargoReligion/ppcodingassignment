2fc2ac5384593ec75354a5569e6bd76e4057c18c
Mahesh <maheshan.vattekat@gmail.com>
2024-09-05 16:48:40 -0400

    try this way

--- Documentmanager.Api/Program.cs
+++ Documentmanager.Api/Program.cs
@@ -1,20 +1,41 @@
+using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using Documentmanager.Core.Domain.Models.Organizations;
+using Documentmanager.Core.Domain.Repositories.Interfaces;
+using Documentmanager.Core.Domain.Repositories.Organizations;
 
-namespace Documentmanager.Api
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Use Autofac as the ServiceProvider
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+// Register services directly with Autofac here
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    // Register your services here
+    containerBuilder.RegisterType<OrganizationRepository>().As<IRepository<Organization>>().InstancePerLifetimeScope();
+    containerBuilder.RegisterType<OrganizationRepository>().As<IOrganizationRepository>().InstancePerLifetimeScope();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    // You can also register entire assemblies
+    // containerBuilder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+    //     .AsImplementedInterfaces();
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
